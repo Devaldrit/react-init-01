74135409a60c00ebd80e3eda2f678ec6fb5e812f
Diti <99714219+Devaldrit@users.noreply.github.com>
2024-12-03 15:19:17 +0100

    mise en place affichage en liste

--- src/App.js
+++ src/App.js
@@ -6,11 +6,37 @@ import {About} from "./components/About/About";
 import {Card} from "./components/Card/Card";
 
 
+const users = [
+  {
+    imgLink:"https://i.pravatar.cc/300?img=11",
+    name:"Maxime Faubert",
+    description:"Développeur Front-end passionné par les interfaces élégantes et réactives."
+  },
+  {
+    imgLink:"https://i.pravatar.cc/300?img=10",
+    name:"Emilie Goudreau",
+    description:"Designer créatif avec un œil pour le détail et l'harmonie des couleurs. "
+  },
+  {
+    imgLink:"https://i.pravatar.cc/300?img=9",
+    name:"Alice Lafond",
+    description:"Chef de projet avec une expertise en gestion agile et communication efficace"
+  },
+  {
+    imgLink:"https://i.pravatar.cc/300?img=8",
+    name:"Paul Guédry",
+    description:"Développeur Full-stack aimant résoudre des problèmes complexes avec du code propre. "
+  }
+]
 
-
-
-
-
+const userTableau = users.map(function(user){
+    return <Card
+      imgLink={user.imgLink}
+      name={user.name}
+      photo={user.photo}
+      description={user.description}
+    />
+})
 
 function App() {
   return (
@@ -19,10 +45,7 @@ function App() {
       <Header></Header>
       <About></About>
       <div className={classes["cards-container"]}>
-          <Card imgLink="https://i.pravatar.cc/300?img=11" name="Maxime Faubert" description="Développeur Front-end passionné par les interfaces élégantes et réactives." />
-          <Card imgLink="https://i.pravatar.cc/300?img=10" name="Emilie Goudreau" description="Designer créatif avec un œil pour le détail et l'harmonie des couleurs. " />
-          <Card imgLink="https://i.pravatar.cc/300?img=9" name="Alice Lafond" description="Chef de projet avec une expertise en gestion agile et communication efficace" />
-          <Card imgLink="https://i.pravatar.cc/300?img=8" name="Paul Guédry" description="Développeur Full-stack aimant résoudre des problèmes complexes avec du code propre. " />
+          {userTableau}
       </div>
       <Footer></Footer>
     </div>
